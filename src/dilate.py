f99dbf7de56352ca2bb6c600a4993bf3cc4bb874
V1ncenttt <vincentlfve@gmail.com>
2024-07-24 15:47:40 +0100

    Added script to dilate all segmentations

--- src/dilate.py
+++ src/dilate.py
@@ -1,6 +1,36 @@
 from pyNiftyReg.utils import *
+ITERS  = 3
 
 if __name__ == '__main__':
-    seg = load_volume('../data/segmentations/summit-2455-xab_Y0_BASELINE_A_airway.nii.gz')
-    dilated_seg = dilate(seg, iterations=3)
-    nib.save(nib.Nifti1Image(dilated_seg.astype(np.int32), np.eye(4)), 'output_24552/dilated_seg_24552_pasdefo_Y0.nii.gz')
+    segs_dir = "../data/segmentations/"
+    segs = list_nii_gz_files(segs_dir)
+    baseline_segs = sorted([seg for seg in segs if "Y0" in seg])
+    y2_segs = sorted([seg for seg in segs if "Y2" in seg])
+    patients = list(zip(baseline_segs, y2_segs))
+
+    original_nifti = nib.load('../data/nii_dataset/1')
+    original_affine = original_nifti.affine
+
+    for patient in patients:
+        identifier = "".join([ele for ele in patient[0] if ele.isdigit()])[:-1]
+
+        registered_seg = 'output_%s2/updated_resampled_seg_f3d_%s2.nii.gz' % (identifier, identifier)
+        registered_seg_back = 'output_%s0/updated_resampled_seg_f3d_back_%s0.nii.gz' % (identifier, identifier)
+
+        baseline_seg = patient[0]
+        y2_seg = patient[1]
+
+        for seg in [registered_seg, registered_seg_back, baseline_seg, y2_seg]:
+            if not seg.startswith('../'):
+                output_name = seg.split('.')[0] + '_dilated_%s.nii.gz' % ITERS
+            else:
+                output_name = segs_dir + seg.split('/')[-1].split('.')[0] + '_dilated_%s.nii.gz' % ITERS
+
+            print(seg)
+            print(output_name)
+            print('---')
+
+            vol = load_volume(seg)
+            dilated_vol = dilate(vol, iterations=ITERS)
+            nib.save(nib.Nifti1Image(dilated_vol.astype(np.int32), np.eye(4)), output_name)
+                                 
