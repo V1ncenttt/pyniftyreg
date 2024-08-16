8738277367dc2e10339c22ca966ddac83b867d59
V1ncenttt <vincentlfve@gmail.com>
2024-08-16 16:26:04 +0100

    refactored module

--- src/pyNiftyReg/__init__.py
+++ src/pyNiftyReg/__init__.py
@@ -3,3 +3,5 @@ from .f3d import F3d
 from .registrator import Registrator
 from .utils import show_slice, show_slice_series
 from .transformer import Transformer
+from .airway_segment_rematcher import AirwaySegmentRematcher
+from .volume_operator import Operator
